a8ea29b239abea7a575dcd72ae4ea7eef7e3f6d8
emilton <emiltonnhampossa0@gmail.com>
2023-04-23 19:02:24 +0200

    chore:Inicializacao do projecto e definicao das paginas Home e About

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Twitter</h4>
+
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ex non eligendi itaque veniam tempore hic. Aliquam, repellat at est, id quae veniam facilis soluta tenetur exercitationem quidem veritatis quisquam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque facilis odio obcaecati hic quia praesentium, quaerat sed, officia blanditiis accusantium repellat repellendus ullam sequi quasi odit neque molestias quibusdam quod!</p>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+        bottom-slots
+        v-model="NewTueetContent"
+        label="O que estas a Pensar?"
+        class="new-tweet"
+        counter
+        maxlength="280"
+        :dense="dense"
+        autogrow
+        >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </template>
+      </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          :disable="!NewTueetContent"
+            unelevated
+            rounded
+            class="q-mb-lg"
+            color="primary"
+            label="Tweet"
+            no-caps
+            />
+      </div>
+    </div>
+    <q-separator
+    class="divider"
+      size="10px"
+      color="grey-2" />
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageHome',
+  data(){
+    return{
+      NewTueetContent: ''
+    }
+
+  }
+})
+</script>
+
+<style lang="sass">
+.new-tweet
+  textarea
+  font-size:19px
+    line-height: 1.4 !important
+
+  .divider
+    border-top:1px solid
+      border-bottom:1px solid
+        border-color: $grey-4
+</style>
