3cb01ec335b345bf63ff36d0ff9e3ddec5dd8674
emilton <emiltonnhampossa0@gmail.com>
2023-04-23 20:33:21 +0200

    chore:adicionando postes na pagina home

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -36,19 +36,96 @@
       size="10px"
       color="grey-2" />
 
+      <q-list separator>
+      <q-item
+        v-for="tweet in tweets"
+        :key="tweet.date"
+        class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"><strong>Emilton Nhampossa</strong>
+            <span class="text-grey-7">
+              @emilton_nhampossa
+            </span>
+          </q-item-label>
+          <q-item-label class="tweet-content text-body1">
+            {{ tweet.content }}
+          </q-item-label>
+          <div class="tweet-icons row justify-between q-mt-sm">
+            <q-btn
+              flat
+              size="sm"
+              round
+              icon="far fa-comment"
+              color="grey"
+               />
+
+              <q-btn
+              flat
+              size="sm"
+              round
+              icon="fas fa-retweet"
+              color="grey"
+               />
+
+              <q-btn
+              flat
+              size="sm"
+              round
+              icon="far fa-heart"
+              color="grey"
+              />
+
+              <q-btn
+              flat
+              size="sm"
+              round
+              icon="fas fa-trash"
+              color="grey" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          {{ tweet.date || relativeDate }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { formatDistance} from 'date-fns'
 
 export default defineComponent({
   name: 'PageHome',
   data(){
     return{
-      NewTueetContent: ''
+      NewTueetContent: '',
+      tweets:[
+        {
+          content:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur nulla officiis et cum eius. Commodi impedit earum illo enim, itaque vitae autem libero non suscipit eos, corrupti velit laudantium perspiciatis!',
+           date:1682272626235
+        },
+        {
+          content:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur nulla officiis et cum eius. Commodi impedit earum illo enim, itaque vitae autem libero non suscipit eos, corrupti velit laudantium perspiciatis!',
+          date:1682272626235
+        },
+
+      ]
     }
 
+  },
+  filters:{
+    relativeDate(value){
+      return formatDistance(value, new Date())
+    }
   }
 })
 </script>
@@ -62,5 +139,12 @@ export default defineComponent({
   .divider
     border-top:1px solid
       border-bottom:1px solid
-        border-color: $grey-4
+        border-color: $grey-2
+
+    .tweet-content
+       white-space:pre-line
+
+       .tweet-icons
+        margin-left: 5px
+
 </style>
